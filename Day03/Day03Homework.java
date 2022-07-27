0bed8b87b10e05a935be1ab3986eeeda36af9126
Helentran893 <88301343+Helentran893@users.noreply.github.com>
2022-07-27 13:03:35 +0700

    Update Day03Homework.java

--- Day03/Day03Homework.java
+++ Day03/Day03Homework.java
@@ -14,6 +14,27 @@ public class Day03Homework {
             }
         } while(isEven == false);
         
+        //Bai 2//
+        int number =0;
+		int count =0;
+		int sum = 0;
+		for(number=2;number<=100;number++)
+		{
+		count =0;
+		for (int i = 2; i <= number/2; i++)
+				{
+					if(number % i == 0)
+					{
+						count++;
+						break;
+					}
+				}
+			if(count == 0)
+				{
+					sum = sum + number;
+				}  
+		}
+		System.out.println(sum);
 
         //Bai 3//
         int digitSum = 0;
