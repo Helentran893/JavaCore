ed215b74a3eebc052d7e40d22b45a244d7e403bb
Helentran893 <88301343+Helentran893@users.noreply.github.com>
2022-08-03 06:24:53 +0700

    Create Day04Homework.java

---
+++ Day04Homework.java
@@ -0,0 +1,154 @@
+import java.util.Scanner;
+
+public class HomeworkDay04 {
+
+    // phuc vu cho bai 03,04
+    // public static boolean isPrimeNumber(int n) {
+    // if (n < 2) {
+    // return false;
+    // }
+    // int squareRoot = (int) Math.sqrt(n);
+    // for (int i = 2; i <= squareRoot; i++) {
+    // if (n % i == 0) {
+    // return false;
+    // }
+    // }
+    // return true;
+    // }
+    
+    public static void main (String[] args){
+        // bai1a
+        // int i,j;
+        // int input = 4;
+        // for (i=1; i<=input; i++){
+        //     for(j=1; j<=input; j++){
+        //         System.out.print("* ");
+        //     }
+        //         System.out.println();
+        //     }
+        //bai1b
+        int i,j;
+        // int input = 4;
+        // for (i=1; i<=input; i++){
+        //     for(j=0; j<i; j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+        //bai2
+        // String text = "You only live once, but if you do it right, once is enough";
+        // System.out.println(text.length());
+        // int count =0;
+        // for(int i=0; i<text.length(); i++){
+        //     if (text.charAt(i)== 'o') {
+        //         System.out.println("Vi tri: " + i);
+        //         count++;
+        //     }
+        // }
+        //     System.out.println("So lan xuat hien: " + count);
+        //bai3
+        Scanner input = new Scanner(System.in);
+        // System.out.println("Nhap gioi han tren cua so random");
+        // int max = input.nextInt();
+        // System.out.println("Nhap gioi han duoi cua so random");
+        // int min = input.nextInt();
+        // int range = max - min + 1;
+        // int rand = (int)(Math.random() * range) + min;
+        // System.out.println(rand);
+        // System.out.println("True - là số nguyên tố; False - không là số nguyên tố");
+        // System.out.println(isPrimeNumber(rand));
+        //bai4
+    //     System.out.println("Nhap gioi han N tinh cac so nguyen to");
+    //     int N = input.nextInt();
+    //     System.out.println("Liet ke N so nguyen to dau tien");
+    //     int count =0;
+    //     for (int i=1; count < N; i++){
+    //         if(isPrimeNumber(i)) {
+    //             System.out.print(i + " ");
+    //             count++;
+    //     }
+    // }
+
+    //     System.out.println("\n"+"Liet ke cac so nguyen to nho hon gioi han N");
+    //     for(int j=1; j<=N; j++){
+    //         if(isPrimeNumber(j)) {
+    //             System.out.print(j + " ");
+    //         }
+    //     }
+    //Bai 5
+    // int[][] arr;
+    // arr = new int[3][4];
+    // System.out.println("Nhap " + (3 * 4) + " Array Elements : ");
+
+    // for (i = 0; i < 3; i++) {
+    //     for (j = 0; j < 4; j++) {
+    //         arr[i][j] = input.nextInt();
+    //     }
+    // }
+    // System.out.print("The Array is :\n");
+    // for (i = 0; i < 3; i++) {
+    //     for (j = 0; j < 4; j++) {
+    //     System.out.print(arr[i][j] + "  ");
+    //     }
+    //     System.out.println();
+    // }
+
+    // for (i =0; i<3; i++){
+    //     for (j=0; j<4; j++){
+    //         if(i==j){
+    //             System.out.print(arr[i][j]);
+    //         }
+    //     }
+    // }
+   
+    // int[][] arr2;
+    // arr2 = new int[3][4];
+    // System.out.println("\n" + "Nhap " + (3 * 4) + " Array Elements : ");
+
+    // for (i = 0; i < 3; i++) {
+    //     for (j = 0; j < 4; j++) {
+    //         arr2[i][j] = input.nextInt();
+    //     }
+    // }
+    // System.out.print("The Array2 is :\n");
+    // for (i = 0; i < 3; i++) {
+    //     for (j = 0; j < 4; j++) {
+    //     System.out.print(arr2[i][j] + "  ");
+    //     }
+    //     System.out.println();
+    // }
+
+
+    //     int addedArr[][] = new int[3][4];
+    //     for (i = 0; i < 3; ++i) {
+    //             for (j = 0; j < 4; ++j) {
+    //                 addedArr[i][j] = arr[i][j] + arr2[i][j];
+    //                 System.out.println(addedArr[i][j]);
+    //             }
+    //         }
+        
+        
+    //     System.out.println("The addedArr is:");
+    //     for (i = 0; i < 3; i++) {
+    //     for (j = 0; j < 4; j++) {
+    //     System.out.print(addedArr[i][j] + "  ");
+    //     }
+    //     System.out.println();
+    // }
+}
+}
+
+
+      
+
+
+        
+
+
+            
+
+            
+    
+
+
+    
